59b0f898af713181b0400b4f8fe458248e584d4e
Yoss1800 <contacto2000@gmail.com>
2022-12-08 05:59:41 -0300

    Product Cards (Home&P.List) + json + Product List Page - Working v1

---
+++ src/pages/productList.tsx
@@ -0,0 +1,45 @@
+import articulos from '../productListScratchData/articulos.json';
+import CardProductList from '../components/CardProductList';
+
+
+// para traer del back: 
+
+/* export async function getStaticProps(context) {
+    const res = await fetch('..url..');
+    const data = await res.json();
+
+    return {
+        props: {products: data}
+    }
+} */
+
+
+function productlist() {
+    console.log(articulos.productsCollection[0]?.["photo/s"])
+
+    return (
+
+        <div>
+            
+            <h1>esto es un product list</h1>
+
+            <div className='card.prodList'>
+                {
+                    articulos.productsCollection?.map((p) => (
+                   <CardProductList
+                        productName={p.productName}
+                        photo={p["photo/s"]}
+                        productPrice={p.productPrice} id={undefined} 
+                        
+                    />           
+                            
+                        
+                    )) 
+                }
+            </div>    
+        </div>
+    ) 
+  }
+  
+  export default productlist;
+
