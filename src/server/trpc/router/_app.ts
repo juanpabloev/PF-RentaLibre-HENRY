0a3221ca05eb2f2665c613098368a3d0221b22bf
Juan Pablo Evangelista <juanpicap23@gmail.com>
2022-12-11 18:35:32 -0300

    feat(server/trpc): product router get routes done

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,9 +1,9 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
+import { productsRouter } from "./productRouter";
 
 export const appRouter = router({
-  example: exampleRouter,
+  products: productsRouter,
   auth: authRouter,
 });
 
