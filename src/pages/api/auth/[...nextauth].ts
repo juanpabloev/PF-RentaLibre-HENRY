fd70b6f675a90260518a38c5b817af78a1ae3984
Yoss1800 <contacto2000@gmail.com>
2022-12-13 23:30:25 -0300

    :sparkles: Adding Google sign in -nextauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,6 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
+import NextAuth, { type NextAuthOptions } from "next-auth"; //esta es original
+//import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
 import DiscordProvider from "next-auth/providers/discord";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -6,25 +8,54 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
 
+/* export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    })
+    
+  ],
+  secret: process.env.JWT_SECRET,
+
+}) */
+
+///esta de aca abajo venia por defecto:
+
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
-  callbacks: {
+  /*  callbacks: {
     session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
       }
       return session;
     },
-  },
+  }, */
+
   // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
+  //adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
+    /* DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
+    }), */
     // ...add more providers here
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+
+      //Alternatively, you can also pass options in the params object of authorization which will force the Refresh Token to always be provided on sign in, however this will ask all users to confirm if they wish to grant your application access every time they sign in:
+      /* authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      } */
+    }),
   ],
+  secret: process.env.JWT_SECRET,
 };
 
 export default NextAuth(authOptions);
