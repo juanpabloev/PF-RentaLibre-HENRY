552ef3c5e919745b903805ee39ede9f3aca9c575
Jesús Lautaro Careglio Albornoz <j.l.careglio.albornoz@gmail.com>
2022-12-20 13:05:42 -0300

    💄 Nueva barra con info sobre la busqueda + order

--- src/pages/productList.tsx
+++ src/pages/productList.tsx
@@ -1,4 +1,3 @@
-import articulos from "../productListScratchData/articulos.json";
 import CardProductList from "../components/CardProductList";
 import styles from "../styles/productList.module.css";
 import React, { useState, useEffect } from "react";
@@ -6,65 +5,38 @@ import { useRouter } from "next/router";
 import { RouterOutputs, trpc } from "../utils/trpc";
 import { FaLeaf } from "react-icons/fa";
 
-// para traer del back:
+import {
+  Box,
+  Flex,
+  Text,
+  Highlight,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from "@chakra-ui/react";
+import { RxCaretDown as ChevronDownIcon } from "react-icons/rx";
 
-/* export async function getStaticProps(context) {
-    const res = await fetch('..url..');
-    const data = await res.json();
-
-    return {
-        props: {products: data}
-    }
-} */
-
-// export const getServerSideProps = async () => {
-//   const res = trpc.product.getProducts.useQuery({ limit: 100, page: 1 }).data;
-//   const productsData = res.data;
-//   return {
-//     props: {
-//       productsData,
-//     },
-//   };
-// };
-
-// interface Item {
-//   id: number;
-//   productName: string;
-//   productPrice: number;
-//   userName: string;
-//   ["photos"]: string;
-//   description: string;
-//   fecha: string;
-//   category: string;
-//   available: boolean;
-// }
-
-// interface Props {
-//   productsData: RouterOutputs["product"]["getProducts"];
-// }
-
-// trae de local host 3001 - json server
-// function bringData(category?: string) {
-//   let data: RouterOutputs["product"]["getProducts"];
-
-//   if (category) {
-//     data = trpc.product.getProductByCategory.useQuery({
-//       categoryName: category,
-//     });
-//   } else {
-//     data = trpc.product.getProducts.useQuery({ page: 1, limit: 10 });
-//   }
-//   return data;
-// }
-//export default function Productlist({ productsData }: Props) {
 export default function Productlist() {
   const router = useRouter();
   const { category, q } = router.query;
+
   //trae del back
   const utils = trpc.useContext();
   let products;
   if (category || q) {
-    console.log(category);
+    // console.log(category);
     if (category && !(typeof q == undefined)) {
       products = trpc.product.getProductByCategory.useQuery({
         categoryName: category,
@@ -81,8 +53,7 @@ export default function Productlist() {
     products = trpc.product.getProducts.useQuery({ limit: 10, page: 1 }).data;
   }
   const [data, setData] = useState();
-  // const [isLoading, setLoading] = useState(false);
-  // if (isLoading) return <p>Loading...</p>;
+  const [order, setOrder] = useState("Más relevantes");
   //para que refresque los datos
   useEffect(() => {
     setData(products);
@@ -92,6 +63,45 @@ export default function Productlist() {
 
   return (
     <div>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={8} alignItems={"center"} justifyContent={"space-between"}>
+          <Flex alignItems={"center"}>
+            <Highlight
+              query={`${q}`}
+              styles={{ px: "1", py: "0", rounded: "full", bg: "orange.100" }}
+            >
+              {`1 a ${data?.length} de ${data?.length} resultados para: ${q}`}
+            </Highlight>
+          </Flex>
+          <Flex alignItems={"center"}>
+            <Menu>
+              <MenuButton
+                as={Button}
+                rounded={"full"}
+                variant={"link"}
+                cursor={"pointer"}
+                minW={0}
+              >
+                Ordenar por: {order}
+              </MenuButton>
+              <MenuList zIndex={2}>
+                <MenuItem onClick={() => setOrder("Más relevantes")}>
+                  Más relevantes
+                </MenuItem>
+                <MenuDivider />
+                <MenuItem onClick={() => setOrder("Menor precio")}>
+                  Menor precio
+                </MenuItem>
+                <MenuDivider />
+                <MenuItem onClick={() => setOrder("Mayor precio")}>
+                  Mayor precio
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </Flex>
+      </Box>
+
       <div className={styles.cardsDivProdHome}>
         {data?.map((p) => (
           <CardProductList
@@ -106,29 +116,3 @@ export default function Productlist() {
     </div>
   );
 }
-
-// esta de aca abajo trae de JSON local
-/*   function productlist() {
-    
-    console.log(articulos.productsCollection[0])
-
-    return (
-        <div>
-            <h1>esto es un product list</h1>
-            <div className={styles.cardsDivProdHome}>
-                {
-                    articulos.productsCollection?.map((p) => (
-                   <CardProductList
-                        productName={p.productName}
-                        photo={p["photo/s"]}
-                        productPrice={p.productPrice} 
-                        id={p.id}         
-                    />           
-                    )) 
-                }
-            </div>    
-        </div>
-    ) 
-  } */
-
-//export default productlist;
