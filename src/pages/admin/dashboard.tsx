e186c449be4610ace0b899d01cbd48c788db712a
Yoss1800 <contacto2000@gmail.com>
2023-01-03 13:08:11 -0300

    redirects to 404 better

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -2,31 +2,27 @@ import React from "react";
 import { useSession, signIn, signOut, getSession } from "next-auth/react";
 import { useRouter } from 'next/router'
 
+import ErrorPage from 'next/error'
+
+
 
 const Dashboard = () => {
-   const { data: session, status } = useSession({ required: true })
-   const router = useRouter()
-    
-   //console.log(session); //me va a dar  objeto con la info de la sesion
+  const { data: session, status } = useSession({ required: true })
+  const router = useRouter()
+
+  //console.log(session); //me va a dar  objeto con la info de la sesion
 
   if (status === "authenticated" && session?.userDB.role === 'ADMIN') {
-   return (
-     <div>
-       <p>Soy Admin - Dashboard</p>
-       <p>{session.user?.name}, SOS ADMINISTRADOR</p>
-       
-     </div>
-   );
- } else {
-  router.push('/404')
-  /* return (
-    <div>
-      <p>SOS UN 404!!! test</p>
-      
-    </div>
-  ); */
- }
-};
+    return (
+      <div>
+        <p>Soy Admin - Dashboard</p>
+        <p>{session.user?.name}, SOS ADMINISTRADOR</p>
 
+      </div>
+    );
+  } else {
+    return <ErrorPage statusCode={404} />
+  }
+};
 
 export default Dashboard;
