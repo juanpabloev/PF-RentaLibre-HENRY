9bf2ff3a80b1d91b2b6419b61b4c8253bf917b25
ulises Gomez <ulisescap123@gmail.com>
2023-01-12 06:17:33 -0300

    :bug: now admins can see the dashboard

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -9,10 +9,9 @@ const Dashboard = () => {
   const { data: session, status } = useSession({ required: true })
   const router = useRouter()
 
-  if (status === "authenticated" && session?.userDB.role === 'ADMIN' && session?.userDB?.banned) {
+  if (status === "authenticated" && session?.userDB.role === 'ADMIN' && !session?.userDB?.banned) {
     return (
       <div>
-        <p>Soy Admin - Dashboard</p>
         <p>{session.user?.name}, SOS ADMINISTRADOR</p>
         <Admin/>
       </div>
